b7f3e82a774515d1ff6aa86e683d7c91229e2261
Marcos Rezk <marcos.rezk@sonda.com>
2023-08-03 22:22:10 -0300

    se modifica el listado usuarios

--- static/js/usuarios.js
+++ static/js/usuarios.js
@@ -25,23 +25,25 @@ async function cargarUsuarios() {
   const request = await fetch("http://localhost:8080/usuarios", {
     method: 'GET',
   })
-  let response = await request.json()
-  console.log(response)
+  const usuarios = await request.json();
+  console.log(usuarios)
   if (request.ok) {
-            let text = ``
-            response.forEach(usuario => {
-                text +=
-                    `
-                     <tr>
-                        <th>${usuario.id}</th>
-                        <th>${usuario.email}</th>
-                        <th>${usuario.name}</th>
-                    </tr>
-                    `
-            });
-            document.getElementById("usuariosTBody").innerHTML = text;
-  }
-document.querySelector('#usuarios tbody').outerHTML = listadoHtml;
+            
+            let listadoHtml = '';
+            for (let usuario of usuarios) {
+  
+              let botonEliminar = '<a href="#" onclick="eliminarUsuario(' + usuario.id + ')" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a>' ;
+              let botonEditar = '<a href="#" onclick="editarUsuario(' + usuario.id +')" btn btn-info btn-circle btn-sm"><i class="fas fa-info-circle"></i></a>  | ' ;
+              let usuarioHtml = '<tr><td>'+ usuario.id+'</td><td>' + usuario.email + '</td><td>' + usuario.name + '</td><td>'
+                             +'</td><td>'+ botonEditar 
+                               + botonEliminar + '</td></tr>';
+              listadoHtml += usuarioHtml;
+              }
+            
+            
+            document.querySelector('#usuariosTBody').outerHTML = listadoHtml;
+            
+            }
 
 }
 
