701c7d67537e122e0b0fe6b56c9c3f528a1b1838
Marcos Rezk <marcos.rezk@sonda.com>
2023-08-22 21:02:32 -0300

    se modifica app.go

--- utils/api/app.go
+++ utils/api/app.go
@@ -24,7 +24,7 @@ func (a *API) Start(e *echo.Echo, address string) error {
 	a.RegisterRoutes(e)
 
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins:     []string{":23.22.45.156:5000"},
+		AllowOrigins:     []string{"23.22.45.156:5000"},
 		AllowMethods:     []string{echo.POST, echo.GET, echo.PUT, echo.DELETE},
 		AllowHeaders:     []string{echo.HeaderContentType},
 		AllowCredentials: true,
