0bd6e7a56a534a8e24db0b88182b8c28e62a00c4
santiago lamath <santimil999@gmail.com>
2023-08-30 19:43:46 -0300

    SA Lista Documentos

---
+++ static/js/documentosDeObra.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // getDocumentos()
+
+    $('#documentos').DataTable();
+});
+
+
+
+
+function redirectCrearDocumento(){
+    window.location.href = `altaDePlan.html`;
+}
