0a563292cca56a4d847cc687c70a3542210bf929
santiago lamath <santimil999@gmail.com>
2023-09-03 14:18:15 -0300

    SA lista documentos 1

--- static/js/documentosDeObra.js
+++ static/js/documentosDeObra.js
@@ -4,7 +4,32 @@ $(document).ready(function() {
     $('#documentos').DataTable();
 });
 
+async function getControles() {
 
+    let obraid = null;
+    const url = new URL(document.URL);
+    const searchParams = url.searchParams;
+    obraid = searchParams.get('obra_id');
+
+    const request = await fetch("http://localhost:5000/documentos", {
+        method: 'GET',
+    })
+
+    const documentos = await request.json();
+    console.log(documentos)
+    if (request.ok) {
+        let listadoHtml = '';
+            for (let documento of documentos) {
+  
+              let documentoHtml = '<tr><td>'+ documento.ID +'</td><td>' + documento.Formulario + '</td><td>' + documento.Piso + '</td></tr>';
+              listadoHtml += documentoHtml;
+              }
+            
+            
+            document.querySelector('#documentosTBody').outerHTML = listadoHtml;
+            
+            }
+    }
 
 
 function redirectCrearDocumento(){
